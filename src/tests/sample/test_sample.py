79bb2e334290ab06713341f267d2b8b04c9813d9
Promode <pramoddutta_96@yahoo.com>
2025-12-18 07:56:04 +0530

    First

--- src/tests/sample/test_sample.py
+++ src/tests/sample/test_sample.py
@@ -1,10 +1,11 @@
 import pytest
 import allure
-
+@allure.step("Step 1")
 @allure.title("Verify 🛑 Failed !that the Framework is working")
 def test_sample_pass():
     assert True == False
 
 @allure.title("Verify ✅ Passed that the Framework is working")
+@allure.step("Step 2")
 def test_sample_fail():
     assert True == True
